57d68876f9202a85c09a2d01c972e2db35155cd5
Luke Dillman <Dillman@Lukes-iMac.local>
2016-07-03 14:07:15 -0300

    Added files got github

--- RedBlue/ViewController.swift
+++ RedBlue/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var blackTriforce: UIImageView!
+    
+    @IBOutlet weak var goldTriforce: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+
+    @IBAction func hideBlackTriforce(sender: AnyObject) {
+        
+        blackTriforce.hidden = true
+    }
+    
+   
+
+    @IBAction func hideGoldTriforce(sender: AnyObject) {
+        goldTriforce.hidden = true
+    }
 
 }
 
